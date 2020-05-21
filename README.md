61831387d0ef9d440fb6c159a0bb5a9c3f577d5d
Stiven Rodriguez <merq.rodriguez@udla.edu.co>
2020-05-21 12:23:35 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+
+
+## TensorflowJS + ReactJS - Object Detector Mobilenet
+
+![Drag Racing](src/tensor.png)
+
+In the project directory, you can run:
+
+```bash
+  $ yarn install
+```
+
+```bash
+  $ yarn start
+```
+
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+![Drag Racing](src/cap.png)
+
+### Consult Tensorflow models
+> https://github.com/tensorflow/tfjs-models
+
+
---
+++ src/App.js
@@ -0,0 +1,98 @@
+import React, { useState, useRef, useReducer } from "react";
+import * as mobilenet from "@tensorflow-models/mobilenet";
+import "./App.css";
+
+const machine = {
+  initial: "initial",
+  states: {
+    initial: { on: { next: "loadingModel" } },
+    loadingModel: { on: { next: "modelReady" } },
+    modelReady: { on: { next: "imageReady" } },
+    imageReady: { on: { next: "identifying" }, showImage: true },
+    identifying: { on: { next: "complete" } },
+    complete: { on: { next: "modelReady" }, showImage: true, showResults: true }
+  }
+};
+
+function App() {
+  const [results, setResults] = useState([]);
+  const [imageURL, setImageURL] = useState(null);
+  const [model, setModel] = useState(null);
+  const imageRef = useRef();
+  const inputRef = useRef();
+
+  const reducer = (state, event) => machine.states[state].on[event] || machine.initial;
+
+  const [appState, dispatch] = useReducer(reducer, machine.initial);
+  const next = () => dispatch("next");
+
+  const loadModel = async () => {
+    next();
+    const model = await mobilenet.load();
+    setModel(model);
+    next();
+  };
+
+  const identify = async () => {
+    next();
+    const results = await model.classify(imageRef.current);
+    console.log(results)
+    setResults(results);
+    next();
+  };
+
+  const reset = async () => {
+    setResults([]);
+    next();
+  };
+
+  const upload = () => inputRef.current.click();
+
+  const handleUpload = event => {
+    const { files } = event.target;
+    if (files.length > 0) {
+      const url = URL.createObjectURL(event.target.files[0]);
+      setImageURL(url);
+      next();
+    }
+  };
+
+  const actionButton = {
+    initial: { action: loadModel, text: "Cargar Modelo" },
+    loadingModel: { text: "Cargando modelo..." },
+    modelReady: { action: upload, text: "Subir imagenes" },
+    imageReady: { action: identify, text: "Identificar objetos" },
+    identifying: { text: "Identificando.." },
+    complete: { action: reset, text: "Reiniciar" }
+  };
+
+  const { showImage, showResults } = machine.states[appState];
+
+  const renderTable = (array) => {
+    return (
+      <ul>
+          {array.map(({ className, probability }) => (
+            <li key={className}>{`${className}: %${(probability * 100).toFixed(
+              2
+            )}`}</li>
+          ))}
+      </ul>
+    )
+  }
+
+  return (
+    <div>
+      {!showImage && <img src={require('./tensor.png')}  />}
+      {showImage && <img src={imageURL} alt="upload-preview" ref={imageRef} />}
+      <input type="file" accept="image/*" capture="camera" onChange={handleUpload} ref={inputRef}/>
+      {showResults && (
+        renderTable(results)
+      )}
+      <button onClick={actionButton[appState].action || (() => {})}>
+        {actionButton[appState].text}
+      </button>
+    </div>
+  );
+}
+
+export default App;
